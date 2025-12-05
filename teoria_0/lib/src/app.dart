3db4072bc8558e5da1776a6d36a35330ac0181eb
Juan_Diego <juan3312orrego@gmail.com>
2025-12-05 18:52:50 -0500

    Aplicación básica de contador

--- teoria_0/lib/src/app.dart
+++ teoria_0/lib/src/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:teoria_0/src/pages/home.dart';
+import 'package:teoria_0/src/pages/counter.dart';
 
 // Widget sin estado
 class MyApp extends StatelessWidget {
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue
       ),
       home: Center(
-        child: Home(),
+        child: Counter(),
       ),
     );
   }
