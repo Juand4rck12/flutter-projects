f2c77823735bdfadfc8f4b7ba2ed347703f78070
Juan_Diego <juan3312orrego@gmail.com>
2025-12-05 19:08:44 -0500

    Mostrar imagenes locales y de internet

--- teoria_0/lib/main.dart
+++ teoria_0/lib/main.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:teoria_0/src/app.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Imagenes",
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Mostrar imagenes")),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              SizedBox(
+                height: 250.0,
+                width: 250.0,
+                child: Image.asset('assets/cat.jpg', fit: BoxFit.cover),
+              ),
+
+              SizedBox(
+                height: 250.0,
+                width: 250.0,
+                child: Image.network(
+                  'https://i.programmerhumor.io/2025/03/0366215f9d522a201fddf2849bb3cac7.jpeg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
