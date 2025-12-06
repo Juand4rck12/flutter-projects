7f6918021a1250a88cd576f83614d2fe523cad91
Juan_Diego <juan3312orrego@gmail.com>
2025-12-06 13:27:30 -0500

    Padding en Flutter

--- teoria_0/lib/pages/containerPage.dart
+++ teoria_0/lib/pages/containerPage.dart
@@ -9,11 +9,35 @@ class ContainerPage extends StatelessWidget {
       style: TextStyle(fontSize: 25.0),
       child: Center(
         child: Container(
-          width: 400.0,
+
+          width: 200.0,
           height: 200.0,
           color: Colors.red,
-          child: Center(child: Text("Hola Flutter",
-          )),
+
+          // Padding only, para especificar direcciones
+          padding: EdgeInsets.only(
+            top: 25.0,
+            bottom: 25.0,
+            right: 15.0,
+            left: 15.0
+          ),
+
+          // Padding all para mismo valor en todas las direcciones
+          // padding: EdgeInsets.all(25),
+
+          // Padding simetrico para especificar lados
+          // Verticales y horizontales
+          // padding: EdgeInsets.symmetric(
+          //   vertical: 10.0,
+          //   horizontal: 10.0
+          // ),
+
+          child: Container(
+            width: 200.0,
+            height: 200.0,
+            color: Colors.amber,
+            alignment: Alignment.center,
+            child: Text("Hola Flutter")),
         ),
       ),
     );
