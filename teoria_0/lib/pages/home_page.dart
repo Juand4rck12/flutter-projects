fd19963dd92bf4645487a18e84ab382cc2dd8f27
Juan_Diego <juan3312orrego@gmail.com>
2026-01-05 18:37:43 -0500

    Manejo de animaciones con opacidad

--- teoria_0/lib/pages/home_page.dart
+++ teoria_0/lib/pages/home_page.dart
@@ -14,39 +14,49 @@ class _HomeState extends State<Home> {
   double _height = 50;
   Color _color = Colors.green;
   BorderRadiusGeometry _borderRadius = BorderRadiusGeometry.circular(8.0);
+  // Opacidad
+  double opacityLevel = 1.0;
+  void changeOpacity() {
+    setState(() {
+      final random = Random();
+      _width = random.nextInt(300).toDouble();
+      _height = random.nextInt(300).toDouble();
+      _color = Color.fromRGBO(
+        random.nextInt(256),
+        random.nextInt(256),
+        random.nextInt(256),
+        1,
+      );
+      _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
+      // Opacidad
+      opacityLevel = opacityLevel == 0 ? 1.0 : 0.0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Animaciones")),
       body: Center(
-        child: AnimatedContainer(
-          width: _width,
-          height: _height,
-          decoration: BoxDecoration(color: _color, borderRadius: _borderRadius),
+        child: AnimatedOpacity(
+          opacity: opacityLevel,
+          duration: const Duration(seconds: 2),
+          child: AnimatedContainer(
+            width: _width,
+            height: _height,
+            decoration: BoxDecoration(
+              color: _color,
+              borderRadius: _borderRadius,
+            ),
 
-          duration: const Duration(seconds: 1),
-          curve: Curves.bounceOut,
+            duration: const Duration(seconds: 1),
+            curve: Curves.bounceOut,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.blue,
-        onPressed: () => {
-          setState(() {
-            final random = Random();
-            _width = random.nextInt(300).toDouble();
-            _height = random.nextInt(300).toDouble();
-            _color = Color.fromRGBO(
-              random.nextInt(256),
-              random.nextInt(256),
-              random.nextInt(256),
-              1,
-            );
-            _borderRadius = BorderRadius.circular(
-              random.nextInt(100).toDouble(),
-            );
-          }),
-        },
+        onPressed: () => {changeOpacity()},
         child: const Icon(Icons.play_arrow, color: Colors.white),
       ),
     );
