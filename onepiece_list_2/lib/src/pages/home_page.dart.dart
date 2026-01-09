92c6e811dd6f792cb4de90431452c1315131e0dd
Juan_Diego <juan3312orrego@gmail.com>
2026-01-08 19:23:17 -0500

    Header finalizado

---
+++ onepiece_list_2/lib/src/pages/home_page.dart.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:onepiece_list_2/src/widgets/header_widget.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 16, 16, 16),
+      body: Column(
+        children: <Widget>[
+          HeaderWidget()
+        ],
+      ),
+    );
+  }
+}
