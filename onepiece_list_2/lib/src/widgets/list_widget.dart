36d5c2f5a2b67a9d8015b357fa4cd333a15e043a
Juan_Diego <juan3312orrego@gmail.com>
2026-01-11 15:50:33 -0500

    Cuerpo de la aplicación - P1

--- onepiece_list_2/lib/src/widgets/list_widget.dart
+++ onepiece_list_2/lib/src/widgets/list_widget.dart
@@ -40,12 +40,64 @@ class _CharactersListState extends State<CharactersList> {
             color: Color.fromARGB(255, 123, 123, 123),
           ),
           const SizedBox(height: 15.0),
+          charactersPageBlocks("nombre", 0xff21E295, "o1"),
+          charactersPageBlocks("nombre", 0xff21E295, "o1"),
+          charactersPageBlocks("nombre", 0xff21E295, "o1"),
+          charactersPageBlocks("nombre", 0xff21E295, "o1"),
+          charactersPageBlocks("nombre", 0xff21E295, "o1"),
+          charactersPageBlocks("nombre", 0xff21E295, "o1"),
         ],
       ),
     );
   }
 
-  Column frontPageBlocks(String image, String title, String subtitle) {
+  Widget charactersPageBlocks(String name, int color, String image) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 5.0),
+      margin: const EdgeInsets.only(bottom: 20.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15.0),
+        color: const Color.fromARGB(66, 43, 43, 43),
+      ),
+      height: 65.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(
+                      blurRadius: 8.0,
+                      offset: const Offset(0, 5),
+                      spreadRadius: 0.0,
+                      blurStyle: BlurStyle.normal,
+                      color: Color(color),
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                padding: const EdgeInsets.all(5.0),
+                child: Image.asset("assets/$image.png"),
+              ),
+              const SizedBox(width: 12.0),
+              Text(
+                name,
+                style: const TextStyle(fontSize: 16.0, color: Colors.white),
+              ),
+            ],
+          ),
+          const IconButton(
+            onPressed: null,
+            icon: Icon(Icons.more_vert_rounded, color: Colors.grey),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget frontPageBlocks(String image, String title, String subtitle) {
     return Column(
       children: [
         ClipRRect(
