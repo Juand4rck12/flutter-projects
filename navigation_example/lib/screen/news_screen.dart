8506e72f99be8849895957299a0c06ca886ef87a
Juan_Diego <juan3312orrego@gmail.com>
2026-01-18 12:27:15 -0500

    Navigator.pop y maybePop - Funcionamiento básico
    Ejemplo básico de sintaxis y enrutamiento usando el pop y maybePop de Navigator

--- navigation_example/lib/screen/news_screen.dart
+++ navigation_example/lib/screen/news_screen.dart
@@ -7,10 +7,39 @@ class NewsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Noticias")),
-      body: const Center(
-        child: Text(
-          "Bienvenido a la sección de noticias",
-          style: TextStyle(fontSize: 18.0),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Bienvenido a la sección de noticias",
+              style: TextStyle(fontSize: 18.0),
+            ),
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Volver usando Pop"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (Navigator.canPop(context)) {
+                  Navigator.maybePop(context);
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      content: Text(
+                        "No hay pantallas previas para regresar",
+                      ),
+                    ),
+                  );
+                }
+              },
+              child: const Text("Volver usando maybePop"),
+            ),
+          ],
         ),
       ),
     );
