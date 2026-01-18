65b468c8548e5bdaaddf767a35fef9b2bc481efd
Juan_Diego <juan3312orrego@gmail.com>
2026-01-18 17:37:42 -0500

    Modal de alerta básico antes de cambiar entre secciones de la aplicación

--- navigation_example/lib/screen/profile_screen.dart
+++ navigation_example/lib/screen/profile_screen.dart
@@ -10,11 +10,37 @@ class ProfileScreen extends StatelessWidget {
       body: Center(
         child: ElevatedButton(
           onPressed: () {
-            Navigator.pushNamed(context, "/settings");
+            _showDialogConfirmation(context);
           },
           child: const Text("Ir a configuracion"),
         ),
       ),
     );
   }
+}
+
+void _showDialogConfirmation(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text("Confirmacion"),
+        content: const Text("¿Estas seguro que quieres ir a configuración?"),
+        actions: [
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text("Cancelar"),
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacementNamed(context, "/settings");
+            },
+            child: const Text("Ir a configuración"),
+          ),
+        ],
+      );
+    },
+  );
 }
