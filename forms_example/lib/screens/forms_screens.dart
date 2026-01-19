b4ef0dd21988ae35e67efa0012272d3adf68ae7f
Juan_Diego <juan3312orrego@gmail.com>
2026-01-18 22:57:43 -0500

    Creación inicial de nuevo proyecto de formularios

---
+++ forms_example/lib/screens/forms_screens.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormsScreens extends StatefulWidget {
+  const FormsScreens({super.key});
+
+  @override
+  State<FormsScreens> createState() => _FormsScreensState();
+}
+
+class _FormsScreensState extends State<FormsScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(body: Text("Formulario"));
+  }
+}
