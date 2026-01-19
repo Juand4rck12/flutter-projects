7b11631e6993eb6be6ff7c0e5732f33a6286e9e3
Juan_Diego <juan3312orrego@gmail.com>
2026-01-19 16:27:13 -0500

    Modelo para Product

---
+++ store_project/lib/features/products/models/product_model.dart
@@ -0,0 +1,38 @@
+class Product {
+  final int id;
+  final String title;
+  final double price;
+  final String description;
+  final List<String> images;
+  final int categoryId;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.images,
+    required this.categoryId,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json["id"],
+      title: json["title"],
+      price: json["price"].toDouble(),
+      description: json["description"],
+      images: List<String>.from(json["images"]),
+      categoryId: json["category"]["id"],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "title": title,
+      "price": price,
+      "description": description,
+      "images": images,
+      "categoryId": categoryId,
+    };
+  }
+}
