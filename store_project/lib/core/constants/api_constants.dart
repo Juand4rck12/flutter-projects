d77e27e2de1030f187711c183a0722ecbe4f634a
Juan_Diego <juan3312orrego@gmail.com>
2026-01-21 17:04:08 -0500

    Nuevo endpoint cargado

--- store_project/lib/core/constants/api_constants.dart
+++ store_project/lib/core/constants/api_constants.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
-  static const String baseUrl = "https://api.escuelajs.co/api/v1";
+  static const String baseUrl = "https://store-backend.devjuan.xyz/api";
   static const String products = "$baseUrl/products";
   static final String? bearerToken = dotenv.env["BEARER_TOKEN"];
 }
