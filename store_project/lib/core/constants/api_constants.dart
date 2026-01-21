0072bc5f9cae273f38179365107d8ed5dc8f0ca7
Juan_Diego <juan3312orrego@gmail.com>
2026-01-21 14:52:37 -0500

    Bearer token añadido

--- store_project/lib/core/constants/api_constants.dart
+++ store_project/lib/core/constants/api_constants.dart
@@ -1,4 +1,7 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class ApiConstants {
   static const String baseUrl = "https://api.escuelajs.co/api/v1";
   static const String products = "$baseUrl/products";
+  static final String? bearerToken = dotenv.env["BEARER_TOKEN"];
 }
