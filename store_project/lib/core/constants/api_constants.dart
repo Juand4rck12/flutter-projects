e56cc9dede67e91e611e38a77985aed3bc19f8f3
Juan_Diego <juan3312orrego@gmail.com>
2026-01-23 08:44:09 -0500

    Nuevos endpoints actualizados

--- store_project/lib/core/constants/api_constants.dart
+++ store_project/lib/core/constants/api_constants.dart
@@ -1,8 +1,9 @@
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ApiConstants {
   static const String baseUrl = "https://store-backend.devjuan.xyz/api";
   static const String products = "$baseUrl/products";
   static const String categories = "$baseUrl/categories";
-  static final String? bearerToken = dotenv.env["BEARER_TOKEN"];
+  static const String auth = "$baseUrl/auth/local";
+  // static final String? bearerToken = dotenv.env["BEARER_TOKEN"];
 }
--- store_project/lib/main.dart
+++ store_project/lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'package:store_project/features/products/providers/cart_provider.dart';
-import 'package:store_project/features/products/providers/login_form_provider.dart';
+import 'package:store_project/features/products/providers/create_product_provider.dart';
+import 'package:store_project/features/auth/login_form_provider.dart';
 import 'package:store_project/features/products/screens/admin_login_screen.dart';
+import 'package:store_project/features/products/screens/create_product_screen.dart';
 import 'package:store_project/features/products/screens/product_list_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
+  // await dotenv.load(fileName: ".env");
   runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => CartProvider()),
         ChangeNotifierProvider(create: (_) => LoginFormProvider()),
+        ChangeNotifierProvider(create: (_) => CreateProductProvider()),
       ],
       child: const MyApp(),
     ),
@@ -29,7 +32,10 @@ class MyApp extends StatelessWidget {
       title: "Fake Store",
       home: const ProductListScreen(),
       debugShowCheckedModeBanner: false,
-      routes: {"/admin-login": (context) => const AdminLoginScreen()},
+      routes: {
+        "/admin-login": (context) => const AdminLoginScreen(),
+        "/create-product": (context) => const CreateProductScreen(),
+      },
     );
   }
 }
